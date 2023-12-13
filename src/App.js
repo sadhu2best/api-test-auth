643625e13723a57480560095855dd11045f515f7
sadhu2best <sudhansu.2best@gmail.com>
2023-12-13 22:01:42 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [repos, setRepos] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchRepos = async () => {
+      try {
+        // Replace 'YOUR_GITHUB_USERNAME' with your GitHub username
+        const username = 'sadhu2best';
+        // Replace 'YOUR_ACCESS_TOKEN' with your GitHub personal access token
+        const accessToken = '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';
+
+        const response = await fetch(`https://api.github.com/users/sadhu2best/repos`, {
+          headers: {
+            Authorization: `token ${accessToken}`,
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch repositories');
+        }
+
+        const repoData = await response.json();
+        setRepos(repoData);
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+
+    fetchRepos();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>GitHub Repositories</h1>
+      {error && <p>Error: {error}</p>}
+      <h2>Repositories:</h2>
+      <ul>
+        {repos.map((repo) => (
+          <li key={repo.id}>{repo.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
